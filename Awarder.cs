5cd46b89cfd4155e4e7e82728e1ef8d7e46ce473
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-02-13 11:51:26 -0600

    done

--- Awarder.cs
+++ Awarder.cs
@@ -2,4 +2,15 @@
 public class Awarder
 {
     // Add your code here
+    public string CalculateAward(points){
+        if(points > 99){
+            return "First Place";
+        }
+        if(points > 49 && points < 100){
+            return "Second Place";
+        } else {
+            return "Participation Award";
+        }
+    }
 }
+
