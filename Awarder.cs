2bfe761cfcceb88986ddfb7740b2adfb0d158f76
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-02-14 16:56:00 -0600

    should work now

--- Awarder.cs
+++ Awarder.cs
@@ -3,14 +3,20 @@ namespace KnightMoves
     public class Awarder
     {
         public int points = 100;
-       public string CalculateAward(points > 99){
-           return "First Place";
-       }
-        else if(points > 49 && points < 100){
-            return "Second Place";
+        public string CalculateAward()
+        {
+            if (points > 99)
+            {
+                return "First Place";
+            }
+            else if (points > 49 && points < 100)
+            {
+                return "Second Place";
+            }
+            else
+            {
+                return "Participation Award";
+            }
         }
-        else{
-            return "Participation Award";
-        };
     }
 }
